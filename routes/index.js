21f3abfc5e27821b76069534c0e2edf6ad22b977
Andrew Cer <cer.andrew@gmail.com>
2015-07-28 16:30:12 -0600

    validation working can check for exsisting username in db otherwise broken

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var validator = require("../lib/validation.js").validation;
+var database = require('../lib/database.js').dbCalls;
+var dbInsert = require('../lib/database.js').dbInsert;
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Tube Clone' });
@@ -12,12 +14,22 @@ router.get('/tube/sign-up', function (req, res) {
 
 router.post('/tube/sign-up', function (req, res) {
   var formData = req.body;
+  var userData = {userName : formData.userName, age : formData.age, password : formData.password}
   var validationArray = validator(formData.userName, formData.password, formData.passwordCheck);
   if (validationArray.length > 0) {
-    res.render('sign-up', {errors: validationArray});
+    res.render('sign-up', {errors: validationArray, formData: formData});
   }
   else {
-    res.redirect('/');
+    database(userData).then(function (obj) {
+      if (obj.userName === userData.userName) {
+        res.render('sign-up', {errors: ['That User Name already exists'], formData: formData});
+      }
+      else {
+        dbInsert(userData).then(function () {
+          res.redirect('/');
+        })
+      }
+    });
   }
 });
 
